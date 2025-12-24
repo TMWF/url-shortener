7236226b5fbe2d40d341869f16105f2e73a67265
Joe <gpliev777@gmail.com>
2025-12-24 17:41:38 +0300

    added mutex to memstorage

--- internal/repository/memstorage.go
+++ internal/repository/memstorage.go
@@ -1,11 +1,14 @@
 package repository
 
 import (
+	"sync"
+
 	"github.com/TMWF/url-shortener/internal/util"
 )
 
 type memStorage struct {
 	urlStorage map[string]string
+	lock       sync.RWMutex
 }
 
 func NewMemStorage() *memStorage {
@@ -13,12 +16,16 @@ func NewMemStorage() *memStorage {
 }
 
 func (ms *memStorage) SaveURL(url string) (string, error) {
+	ms.lock.Lock()
+	defer ms.lock.Unlock()
 	id := util.RandomString(8, util.LatinCharSet)
 	ms.urlStorage[id] = url
 	return id, nil
 }
 
 func (ms *memStorage) GetURL(id string) (string, bool) {
+	ms.lock.RLock()
+	defer ms.lock.RUnlock()
 	url, found := ms.urlStorage[id]
 	return url, found
 }
