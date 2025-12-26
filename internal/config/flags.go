dee546ecb11424d8dd5444c8352f41f0ff957ba7
Joe <gpliev777@gmail.com>
2025-12-26 19:12:56 +0300

    added environment parameters parsing

--- internal/config/flags.go
+++ internal/config/flags.go
@@ -1,6 +1,9 @@
 package config
 
-import "flag"
+import (
+	"flag"
+	"os"
+)
 
 type Config struct {
 	ServerHost string
@@ -8,7 +11,17 @@ type Config struct {
 }
 
 func (cfg *Config) ParseFlags() {
-	flag.StringVar(&cfg.ServerHost, "a", "localhost:8080", "address and port to run server")
-	flag.StringVar(&cfg.BaseURL, "b", "http://localhost:8080", "address and port to run server")
+	cfg.ServerHost = os.Getenv("SERVER_ADDRESS")
+
+	if cfg.ServerHost == "" {
+		flag.StringVar(&cfg.ServerHost, "a", "localhost:8080", "address and port to run server")
+	}
+
+	cfg.BaseURL = os.Getenv("BASE_URL")
+
+	if cfg.BaseURL == "" {
+		flag.StringVar(&cfg.BaseURL, "b", "http://localhost:8080", "address and port to run server")
+	}
+
 	flag.Parse()
 }
