72cb19af5509ec9a5e23ac85508c83bb54545080
Joe <gpliev777@gmail.com>
2025-12-26 23:23:29 +0300

    caarlos usage added

--- internal/config/flags.go
+++ internal/config/flags.go
@@ -2,22 +2,25 @@ package config
 
 import (
 	"flag"
-	"os"
+	"log"
+
+	"github.com/caarlos0/env/v6"
 )
 
 type Config struct {
-	ServerHost string
-	BaseURL    string
+	ServerHost string `env:"SERVER_ADDRESS"`
+	BaseURL    string `env:"BASE_URL"`
 }
 
 func (cfg *Config) ParseFlags() {
-	cfg.ServerHost = os.Getenv("SERVER_ADDRESS")
+	err := env.Parse(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	if cfg.ServerHost == "" {
 		flag.StringVar(&cfg.ServerHost, "a", "localhost:8080", "address and port to run server")
 	}
-
-	cfg.BaseURL = os.Getenv("BASE_URL")
 
 	if cfg.BaseURL == "" {
 		flag.StringVar(&cfg.BaseURL, "b", "http://localhost:8080", "address and port to run server")
