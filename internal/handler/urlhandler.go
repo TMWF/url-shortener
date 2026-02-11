128b4a770c356591c7d0f9478487e3b76a8afe22
Joe <gpliev777@gmail.com>
2026-02-11 09:24:01 +0300

    fixes after failing autotests

--- internal/handler/urlhandler.go
+++ internal/handler/urlhandler.go
@@ -274,6 +274,8 @@ func (h *urlHandler) GetUserURLs(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Length", strconv.Itoa(len(responseBody)))
 	w.WriteHeader(http.StatusOK)
 	w.Write(responseBody)
 }
