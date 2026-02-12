c8506038cb442bc4236cde60170533fd11a2d9dc
Joe <gpliev777@gmail.com>
2026-02-12 14:25:06 +0300

    adding debug logs

--- internal/repository/dbstorage.go
+++ internal/repository/dbstorage.go
@@ -87,10 +87,12 @@ func (dbs *dbStorageImpl) SaveURL(ctx context.Context, url string) (string, erro
 
 		_, err := tx.ExecContext(ctx, "INSERT INTO urls_users (url_id, user_id) VALUES ($1, $2)", urlID, userID)
 		if err != nil {
+			logger.GetLogger().Debug("Error while inserting into urls_users")
 			return "", nil
 		}
 
 		if err = tx.Commit(); err != nil {
+			logger.GetLogger().Debug("Error while commiting transaction")
 			return "", err
 		}
 		logger.GetLogger().Debug("Returning ErrConflict from dbStorage")
